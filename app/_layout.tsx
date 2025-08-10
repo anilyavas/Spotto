78acf5c0304093dc2ca05b0c7f6e24e3c55eeb2d
anilyavas <anil.yavas99@gmail.com>
2025-08-10 20:15:54 +0300

    Starting layout for auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
