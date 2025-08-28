543bdbf27e7ab327a2d26c41e30d2de832a3d11a
anilyavas <anil.yavas99@gmail.com>
2025-08-28 19:32:02 +0300

    ParkingHistory component UI details

--- components/ParkingHistory.tsx
+++ components/ParkingHistory.tsx
@@ -1,4 +1,5 @@
-import { View, Text } from 'react-native';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import { View, Text, Pressable } from 'react-native';
 
 export default function ParkingHistory({
   item,
@@ -11,10 +12,30 @@ export default function ParkingHistory({
   };
 }) {
   return (
-    <View className="bg-gray-300 p-2">
-      <Text>LAT: {item.latitude}</Text>
-      <Text>LON: {item.longitude}</Text>
-      <Text>TIME: {new Date(item.created_at).toLocaleString()}</Text>
+    <View className="m-0.5 flex-row">
+      <View className="flex-1 flex-row justify-between bg-gray-300 p-2">
+        <View className="p-2">
+          <Text className="text-lg font-bold">Location Coords</Text>
+          <Text>
+            <Text className="text-md font-semibold">LAT:</Text> {item.latitude}
+          </Text>
+          <Text>
+            <Text className="text-md font-semibold">LON:</Text> {item.longitude}
+          </Text>
+        </View>
+        <View className="p-2">
+          <Text className="text-lg font-bold">Time & Date</Text>
+          <Text className="text-md font-semibold">
+            {new Date(item.created_at).toLocaleString()}
+          </Text>
+        </View>
+      </View>
+      <Pressable className="items-center justify-center bg-green-600 p-4" onPress={() => {}}>
+        <Fontisto name="navigate" size={20} color={'white'} />
+      </Pressable>
+      <Pressable className="items-center justify-center bg-red-600 p-4" onPress={() => {}}>
+        <Fontisto name="trash" size={20} color={'white'} />
+      </Pressable>
     </View>
   );
 }
